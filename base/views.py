fa32926df0e105598b80db9dffa10b8341410f29
thanakhalid <thanakhalidq@gmail.com>
2024-03-23 23:28:03 +0300

    sign in sign up 2

--- base/views.py
+++ base/views.py
@@ -5,3 +5,7 @@ def show_base(request):
     return render(request, 'base.html')
 def home(request):
     return render(request,'home.html',{})
+def signUp(request):
+    return render(request,'Registration.html')
+def signIn(request):
+    return render(request,'Login.html')
